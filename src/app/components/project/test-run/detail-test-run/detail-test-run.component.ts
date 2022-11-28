c05a59dd27d4120385913356516e8ca747af206c
Nguyen Hoang Son <hoangson99201@gmail.com>
2022-11-29 01:15:54 +0700

    add result

--- src/app/components/project/test-run/detail-test-run/detail-test-run.component.ts
+++ src/app/components/project/test-run/detail-test-run/detail-test-run.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
 import { AddResultComponent } from '../add-result/add-result.component';
 
 @Component({
@@ -7,11 +8,16 @@ import { AddResultComponent } from '../add-result/add-result.component';
   templateUrl: './detail-test-run.component.html',
   styleUrls: ['./detail-test-run.component.scss'],
 })
-export class DetailTestRunComponent {
-  constructor(
-    public dialog: MatDialog
-  ) {
+export class DetailTestRunComponent implements OnInit {
+  public projectId: string = '';
 
+  constructor(public dialog: MatDialog, private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      console.log(params);
+      this.projectId = params['id'];
+      console.log(this.projectId);
+    });
   }
   openDialog() {
     console.log('here');
