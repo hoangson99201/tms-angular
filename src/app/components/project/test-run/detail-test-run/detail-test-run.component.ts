978672515226d4f0798d6d7ec7f19b1e3c30b4db
Nguyen Hoang Son <hoangson99201@gmail.com>
2022-11-29 23:14:28 +0700

    add result

--- src/app/components/project/test-run/detail-test-run/detail-test-run.component.ts
+++ src/app/components/project/test-run/detail-test-run/detail-test-run.component.ts
@@ -10,15 +10,19 @@ import { AddResultComponent } from '../add-result/add-result.component';
 })
 export class DetailTestRunComponent implements OnInit {
   public projectId: string = '';
+  public subId: string = '';
 
   constructor(public dialog: MatDialog, private route: ActivatedRoute) {}
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       console.log(params);
       this.projectId = params['id'];
+      this.subId = params['subId'];
       console.log(this.projectId);
+      console.log(this.subId);
     });
   }
+
   openDialog() {
     console.log('here');
 
--- src/app/models/test-run.ts
+++ src/app/models/test-run.ts
@@ -1,5 +1,6 @@
 export interface TestRun {
   runName: string,
+  runId?: string,
   milestoneId?: number,
   assignedToId?: number,
   description?: string,
