e4596d4a3a1c1d0c92a7fa256cdc92d696e8d6e0
Nguyen Hoang Son <hoangson99201@gmail.com>
2022-12-12 23:46:58 +0700

    add logout and user name

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  public top: string = '';
+  public left: string = '';
+
+  openDropDown(e: any) {
+    var target = e.target || e.srcElement || e.currentTarget;
+    console.log(target.getBoundingClientRect());
+    this.left = target.getBoundingClientRect()['x'] - 1796 + 'px';
+    this.top = target.getBoundingClientRect()['y'] + 1676 + 'px';
+  }
+  closeDropDown() {
+    this.left = '';
+    this.top = '';
+  }
+
+  public getFullname() {
+    console.log('activeUser' + AuthService.activeUser);
+    return AuthService.activeUser.fullname;
+  }
 
 }
