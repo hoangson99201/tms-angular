7cff439cfd4f7d95368e46bf30c0922b789b5fe0
Doan Van Thang <thangdv@onepay.vn>
2022-12-18 20:57:42 +0700

    merge list section API

--- src/app/components/project/testcase/testcase.component.ts
+++ src/app/components/project/testcase/testcase.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { TestCase } from 'src/app/models/test-case';
-import { TestCaseService } from 'src/app/services/test-case.service';
 import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { Section } from 'src/app/models/section';
+import { TestCase } from 'src/app/models/test-case';
+import { SectionService } from 'src/app/services/section.service';
+import { TestCaseService } from 'src/app/services/test-case.service';
 import { SectionDialogComponent } from './section-dialog/section-dialog.component';
 
 @Component({
@@ -14,18 +16,20 @@ export class TestcaseComponent {
   constructor(
     private route: ActivatedRoute,
     private testCaseService: TestCaseService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private sectionService: SectionService
   ) { }
   public projectId: string = '';
   public testCases: TestCase[] = [];
   public map: Map<string, TestCase[]> = new Map<string, TestCase[]>();
+  public sections: Section[] = [];
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       console.log(params);
       this.projectId = params['id'];
       console.log(this.projectId);
       this.testCaseService
-        .findAllByProjectId(+this.projectId)
+        .findAllByProjectId(parseInt(this.projectId))
         .subscribe((testCases) => {
           this.testCases = testCases;
           this.map = new Map<string, TestCase[]>();
@@ -40,6 +44,11 @@ export class TestcaseComponent {
           }
           console.log(testCases);
           console.log(this.map);
+        });
+      this.sectionService.findAllByProjectId(parseInt(this.projectId))
+        .subscribe(sections => {
+          this.sections = sections;
+          console.log(sections);
         });
     });
   }
