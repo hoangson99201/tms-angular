305213ffce77d4f7448eae16cabcceef4fa83aa1
Doan Van Thang <thangdv@onepay.vn>
2023-02-03 22:00:34 +0700

    add project start date end date to dashboard

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -14,10 +14,22 @@ export class AdminDashboardComponent extends BasePaginator implements OnInit {
     this.projectName = projectName;
     this.projectService.getProjects(this.getParams(), projectName).subscribe(reponse => {
       this.projects = reponse.list;
+      this.convertDate(this.projects);
       this.length = reponse.length;
       this.activeProject = this.projects.filter(x => !x.completed).length;
       this.completedProject = this.projects.filter(x => x.completed).length;
     });
+  }
+
+  convertDate(projects: Project[]) {
+    for (const project of projects) {
+      if (project.endDate instanceof Array) {
+        project.endDate = project.endDate[0] + "-" + String(project.endDate[1]).padStart(2, '0') + "-" + String(project.endDate[2]).padStart(2, '0');
+      }
+      if (project.startDate instanceof Array) {
+        project.startDate = project.startDate[0] + "-" + String(project.startDate[1]).padStart(2, '0') + "-" + String(project.startDate[2]).padStart(2, '0');
+      }
+    }
   }
 
   projects: Project[] = [];
